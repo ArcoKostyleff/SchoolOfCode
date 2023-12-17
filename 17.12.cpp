6cf84e8027b4c50fbe485e9a199373a4653ac9cf
Pozetiv4ik <126339417+Poze7iv4ik@users.noreply.github.com>
2023-12-17 11:18:32 +0300

    Add files via upload

---
+++ 17.12.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vector[10];
+
+        for (int ix = 0; ix < 10; ix++)
+            cin >> vector[ix];
+
+    for (int ix = 0; ix < 10; ix++)
+        cout << vector[ix] << endl;
+
+    return 0;
+}
