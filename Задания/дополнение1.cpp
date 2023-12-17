f505c7f0547fd27b78f1bee71ee746d35c919610
AmurTigerr <146886925+AmurTigerr@users.noreply.github.com>
2023-12-17 13:38:07 +0300

    Create дополнение1.cpp

---
+++ Задания/дополнение1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int vector[10];
+    for (int ix = 0; ix < 10; ix++)
+         cin >> vector[ix];
+    for (int ix = 0; ix < 10; ix++)
+        cout << vector[ix] << endl;
+    return 0;
+}
