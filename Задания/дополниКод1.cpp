4be7ba8ac1e599db20797bf65f9163b49cc8b7df
ArcoKostyleff <104649358+ArcoKostyleff@users.noreply.github.com>
2023-12-17 11:11:14 +0300

    Create дополниКод1.cpp

---
+++ Задания/дополниКод1.cpp
@@ -0,0 +1,16 @@
+#include <________>
+ 
+using namespace std;
+ 
+___ main()
+{
+    int vector[10]_
+ 
+    for(int ix = _; ix < _; ___)
+    cin >> ________;
+ 
+    for(___ ix = 0; ___; ___)
+    cout << ________;
+ 
+    return 0;
+}
