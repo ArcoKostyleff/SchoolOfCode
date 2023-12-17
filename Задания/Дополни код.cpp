9bea0cf11a043b956996e7250e7c55822c1f0671
Maxyasnev <122309923+Maxyasnev@users.noreply.github.com>
2023-12-17 11:34:37 +0300

    Create Дополни код.cpp

---
+++ Задания/Дополни код.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int vector[10];
+
+        for (int ix = 0; ix < 10; ix++)
+            cin >> vector[ix];
+
+    for (int ix = 0; ix < 10; ix++)
+        cout << vector[ix];
+
+    return 0;
+}
