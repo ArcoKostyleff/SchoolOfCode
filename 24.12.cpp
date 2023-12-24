4de10847a2eb6323eac7323e2ad89c64781e14d7
Pozetiv4ik <126339417+Poze7iv4ik@users.noreply.github.com>
2023-12-24 13:48:46 +0300

    SFML_trapezoid

---
+++ 24.12.cpp
@@ -0,0 +1,29 @@
+﻿using namespace std;
+#include <iostream>;
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    float w1, w2, h;
+    cin >> w1 >> w2 >> h;
+    sf::RenderWindow window(sf::VideoMode(800, 800), "1");
+    sf::ConvexShape convex;
+    convex.setPointCount(4);
+    convex.setPoint(0, sf::Vector2f{0,h});
+    convex.setPoint(1, sf::Vector2f{(w2-w1)/2 ,0});
+    convex.setPoint(2, sf::Vector2f{((w2 - w1) / 2) + w1,0});
+    convex.setPoint(3, sf::Vector2f{w2,h});
+    
+    window.draw(convex);
+    window.display();
+    
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
+}
