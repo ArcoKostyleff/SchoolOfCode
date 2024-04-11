b9313cd5980a9438c7f51ec1938815da8300cf99
ArcoKostyleff <104649358+ArcoKostyleff@users.noreply.github.com>
2024-04-11 21:04:02 +0300

    Create main.cpp

---
+++ примеры/src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <atomic>
+#include <fstream>
+#include <thread>
+#include <memory>
+#include "aboba.h"
+using namespace std;
+
+int main()
+{
+    aboba("Yasnev");
+    printAboba("Shapar");
+
+}
