124e1792178ab02e436927019b7f29c75dc57ef6
ArcoKostyleff <104649358+ArcoKostyleff@users.noreply.github.com>
2024-04-11 21:03:29 +0300

    Create aboba.cpp

---
+++ примеры/src/aboba.cpp
@@ -0,0 +1,15 @@
+#include "aboba.h"
+
+void aboba() {
+	std::cout << "ty ne Aboba, %USERNAME%!\n";
+}
+
+void aboba(std::string s) {
+	std::cout << "ty ne Aboba, %"<<s<<"%!\n";
+}
+
+
+
+void printAboba(std::string s) {
+	std::cout << s << " is aboba!\n";
+}
